c37b2de54d4377b248b789348d285de874873b88
Alina <70638294+BogAlina@users.noreply.github.com>
2020-09-09 15:56:01 -1100

    First1.0
    Somery

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <time.h>
+#include <math.h>
+#define N 10
+using namespace std;
+
+int main()
+{
+int arr[N][N], temp1=0, temp2=0;
+srand(time(0));
+    for(int i = 0; i<N; i++){
+    for(int j = 0; j<N; j++){
+        arr[i][j]=0+rand()%11;
+        cout << arr[i][j] << "\t";
+}
+cout << "\n";
+}
+
+
+    for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++){
+        if(j<i){
+            temp1+= arr[i][j];
+        }else if(j>i){
+            temp2+= arr[i][j];
+
+        }
+    }
+}
+cout << "\n" << temp1 << "\t" << temp2 << "\t";
+    if(temp1>temp2){
+        cout << "\nlower diagonal";
+    }else if(temp1<temp2){
+        cout << "\nupper diagogonal";
+    }
+    else{
+        cout << "\nlower diagonal = upper dioganal";
+    }
+cout << "\n difference\t" << abs(temp1-temp2);
+    return 0;
+}
